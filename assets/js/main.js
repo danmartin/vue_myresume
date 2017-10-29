4c717c5c0603a6daecbfa7199b8fde79a96acb3e
Dan Martin <dmartin@geiger.com>
2017-10-29 15:46:20 -0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#resume_id',
+    data: {
+        resume: {},
+        isLoaded: false
+    },
+    mounted: function() {
+        this.fetchResume();        
+    },
+    // Methods we want to use in our application are registered here
+    methods: {
+        fetchResume: function() {
+            this.$http.get('/assets/data/resume.json').then(function(response) {
+                this.resume = response.body; 
+                this.isLoaded = true;
+              });
+        }
+    }
+});
