15b54c17847b75970180a5b2cc24fe680321a7fc
Dan Martin <dmartin@geiger.com>
2017-10-30 15:11:15 -0700

    Vue Date formatting and show/hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,38 @@ new Vue({
     el: '#resume_id',
     data: {
         resume: {},
-        isLoaded: false
+        isLoaded: false,
+        bool: {0:false,1:false,2:false,3:false,4:false,5:false,6:false,7:false,8:false,9:false}
     },
     mounted: function() {
         this.fetchResume();        
     },
-    // Methods we want to use in our application are registered here
     methods: {
+        dateFormat: function(date){
+            if(date == 'Present') return date
+            let d = new Date(date),
+                months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                m = months[d.getMonth()];
+            return m + " " + d.getFullYear()
+        },
         fetchResume: function() {
             this.$http.get('/vue_myresume/assets/data/resume.json').then(function(response) {
                 this.resume = response.body; 
                 this.isLoaded = true;
             });
+        },
+        status: function(key) {
+            console.log(this.bool[key])
+            return this.bool[key] === undefined || !this.bool[key] ? "More Details \u00bb" : "\u00ab Less Details";
+        },
+        toggle: function (key) {
+            if(this.bool[key] !== undefined) {
+                this.bool[key] = !this.bool[key];
+            } else {
+                this.bool[key] = true;
+            }
+            console.log(this.bool[key])
+            console.log(this.bool)
         }
     }
 });
